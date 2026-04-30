13ad2f2007ef48c0eca0af72296ec91f82ba963c
CoderCris <crisamer.ortega@gmail.com>
2026-04-30 17:10:59 +0100

    multithread

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() -> Result<(), anyhow::Error> {
-        //
+use rayon::prelude::*;
+mod ports;
+
+fn main() -> Result<()> {
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(256)
+        .build()
+        .unwrap();
+
+    pool.install(|| {
+        let scan_result: Vec<Subdomain> = subdomains::enumerate(&http_client, targer)
+            .unwrap()
+            .into_par_iter()
+            .map(ports::scan_ports)
+            .collect();
+
+        for subdomain in scan_result {
+            println!("{}:", &subdomain.domain);
+            for port in &subdomain.open_ports {
+                println!("      {}", port.port);
+            }
+            println!("");
+        }
+    });
 }
